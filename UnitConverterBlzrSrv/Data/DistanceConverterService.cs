2e223df492f04c7d6582c91b8d7fa7f22931d06f
giacoearly <giacoearly@yahoo.com>
2024-05-29 19:12:03 +0300

    Finished converter with strings variables for miles and kilometers

--- UnitConverterBlzrSrv/Data/DistanceConverterService.cs
+++ UnitConverterBlzrSrv/Data/DistanceConverterService.cs
@@ -4,7 +4,7 @@ public class DistanceConverterService : IDistanceConverter
 {
     public double KilometersToMiles(double kilometers)
     {
-        return kilometers / 1.6093;
+         return kilometers / 1.6093;
     }
 
     public double MilesToKilometers(double miles)
